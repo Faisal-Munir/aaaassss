c86a2eb7b2be4dc62943886db3fcfbfcfefe48ed
Faisal-Munir <engr.faisal2018@gmail.com>
2020-01-27 21:26:12 +0000

    updatedd

---
+++ my_work/target/debug/deps/my_work-02dc5099b9e4c7e8.d
@@ -0,0 +1,5 @@
+/home/faisal/Desktop/Faisal/my_work/target/debug/deps/my_work-02dc5099b9e4c7e8: src/main.rs
+
+/home/faisal/Desktop/Faisal/my_work/target/debug/deps/my_work-02dc5099b9e4c7e8.d: src/main.rs
+
+src/main.rs:
---
+++ my_work/target/debug/my_work.d
@@ -0,0 +1,1 @@
+/home/faisal/Desktop/Faisal/my_work/target/debug/my_work: /home/faisal/Desktop/Faisal/my_work/src/main.rs
